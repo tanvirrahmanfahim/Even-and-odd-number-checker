b1f3787a240d8507b09cba791141471f229fe449
#Fahim <118614639+tanvirrahmanfahim@users.noreply.github.com>
2023-01-03 20:50:26 +0600

    Create .py

---
+++ .py
@@ -0,0 +1,9 @@
+'''odd or even'''
+#Credit goes to tanvir
+a = int(input("Type a number to check: "))
+if a%2==0:
+    print(a,"is a even number")
+else:
+    print(a,"is a odd number")
+    
+print("https://github.com/tanvirrahmanfahim")
